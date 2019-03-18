7473a637dd0ba18e78e29a38e39d78b402f9b907
ko tainen <tainenko@gmail.com>
2019-03-18 10:52:07 +0800

    新增Counting.py的解題思路

--- part1/Counting.py
+++ part1/Counting.py
@@ -18,3 +18,44 @@ a.txt 3
 b.txt 2
 c.jpg 2
 '''
+
+def get_file_name(str):
+    '''
+    輸入str，回傳檔案string
+    :param str:
+    :return filename:
+    '''
+    pass
+
+def count_the_files(urls):
+    '''
+    統計url list中file出現次數，回傳dict
+    :param urls:
+    :return dict:
+    '''
+    pass
+
+def get_top_3_file(dct):
+    '''
+    從統計的dct中取得出現次數前三筆的檔案字串
+    :param dct:
+    :return list:
+    '''
+    pass
+
+if __name__=='__main__':
+    urls = [
+        "http://www.google.com/a.txt",
+        "http://www.google.com.tw/a.txt",
+        "http://www.google.com/download/c.jpg",
+        "http://www.google.co.jp/a.txt",
+        "http://www.google.com/b.txt",
+        "https://facebook.com/movie/b.txt",
+        "http://yahoo.com/123/000/c.jpg",
+        "http://gliacloud.com/haha.png",
+    ]
+    dct=count_the_files(urls)
+    lst=get_file_name(dct)
+    for l in lst:
+        print(l)
+
