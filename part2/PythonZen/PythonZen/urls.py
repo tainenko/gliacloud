9b2826507188a2f34c5b621867f24bac8b671a10
ko tainen <tainenko@gmail.com>
2019-03-18 13:07:46 +0800

    完成djnago server /zen 現在會隨機顯示一句 the Zen of Python

--- part2/PythonZen/PythonZen/urls.py
+++ part2/PythonZen/PythonZen/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from zen import views as zen_views
 
 urlpatterns = [
+    path('zen/',zen_views.index),
     path('admin/', admin.site.urls),
 ]
