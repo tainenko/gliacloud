3e30cd4792a25956f03d318571063c7cb5cba53e
ko tainen <tainenko@gmail.com>
2019-03-18 11:13:47 +0800

    新增Multiplesof3and5的methods與解題思路

--- part1/Multiplesof3and5.py
+++ part1/Multiplesof3and5.py
@@ -4,3 +4,33 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 • Find the sum of all the multiples of 3 or 5 below 1000.
 '''
 
+def get_multi_3(n):
+    '''
+    :param n:
+    :return int:
+    '''
+    pass
+
+def get_multi_5(n):
+    '''
+    :param n:
+    :return int:
+    '''
+    pass
+
+def get_multi_15(n):
+    '''
+    :param n:
+    :return int:
+    '''
+    pass
+
+def get_multi_3_and_5(n):
+    '''
+    :param n:
+    :return int:
+    '''
+    pass
+
+if __name__=='__main__':
+    print(get_multi_3_and_5(1000))
