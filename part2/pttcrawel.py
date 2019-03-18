830a2037db554fccec1995d55f300477350ed0ed
ko tainen <tainenko@gmail.com>
2019-03-18 12:34:58 +0800

    新增查詢內文get_content method 並輸出到content.txt

--- part2/pttcrawel.py
+++ part2/pttcrawel.py
@@ -9,6 +9,12 @@ a) 請用 Python 寫出一個可以爬 ptt /reddit 任意看板（https://www.pt
 '''
 import requests
 from bs4 import BeautifulSoup
+import os
+
+def get_content(url):
+    response = requests.get(url,cookies={'over18': '1'})
+    soup = BeautifulSoup(response.text, 'lxml')
+    return soup.find('div','bbs-screen').getText()
 
 def ptt_crawel(board):
     #所要擷取的網站網址
@@ -24,6 +30,10 @@ def ptt_crawel(board):
     articles = soup.find_all('div', 'r-ent')
 
     filename=board+'.txt'
+    content_file='content.txt'
+
+    if os.path.isfile(content_file):
+        open(content_file, 'w')
     #寫入檔案中
     with open(filename,'w') as f:
         f.write('看板名稱:'+board+'\n')
@@ -37,6 +47,10 @@ def ptt_crawel(board):
 
             print(date,author,title,href)
             f.write('{} {} {} {}{} \n'.format(date,author,title,base_url,href))
+            content = get_content(base_url + href)
+
+            content_txt=open(content_file,'a')
+            content_txt.write(content+'\n')
 
 if __name__=='__main__':
     ptt_crawel('Gossiping')
