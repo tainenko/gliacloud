33b447dacd2bad75c37dc5c5c0cf17bad83c07c8
ko tainen <tainenko@gmail.com>
2019-03-18 11:19:05 +0800

    完成Multiplesof3and5

--- part1/Multiplesof3and5.py
+++ part1/Multiplesof3and5.py
@@ -9,28 +9,43 @@ def get_multi_3(n):
     :param n:
     :return int:
     '''
-    pass
+    sum=0
+    for num in range(n):
+        if not num%3:
+            sum+=num
+    return sum
 
 def get_multi_5(n):
     '''
     :param n:
     :return int:
     '''
-    pass
+    sum = 0
+    for num in range(n):
+        if not num % 5:
+            sum += num
+    return sum
 
 def get_multi_15(n):
     '''
     :param n:
     :return int:
     '''
-    pass
+    sum = 0
+    for num in range(n):
+        if not num % 15:
+            sum += num
+    return sum
 
 def get_multi_3_and_5(n):
     '''
+    15是3和5的最小公倍數
+    欲求得不大於自然數n的3,5倍數總和
+    等於求3的倍數總和+5的倍數總和-15的倍數總和
     :param n:
     :return int:
     '''
-    pass
+    return get_multi_3(n)+get_multi_5(n)-get_multi_15(n)
 
 if __name__=='__main__':
     print(get_multi_3_and_5(1000))
